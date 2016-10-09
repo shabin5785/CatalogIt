608a10054b8e78481991db7f8f4da5f118ff1950
Shabin Hashim <shabin.hashim@gmail.com>
2016-10-09 20:40:55 +0530

    checkin

--- server.js
+++ server.js
@@ -7,8 +7,58 @@ const path = require('path');
 const passport = require('passport');
 const strategy = require('passport-local');
 const jwt = require('express-jwt');
+const bluebird = require('bluebird');
 
 
 //initialize express
+const app = express();
+
+//use body parser to get post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//config app for CORS
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \
+    Authorization');
+    next();
+});
+
+//Mongodb connection
+mongoose.promise = bluebird;
+mongoose.connect(config.database);
+let connection = mongoose.connection;
+
+connection.on('error', console.error.bind(console, 'Connection Error : '));
+connection.on('open',function(){
+	console.log('Connected to database: ' + config.database);
+})
+
+//set public dir to be used
+app.use(express.static(__dirname+'/public'));
+
+//passport
 
 
+//Routes
+var routes = require('./routes/catalog_routes')(app,express);
+
+//Register the routes
+app.use('/catalog',routes);
+
+
+//catch all route
+app.get('*', function(req,res){
+	res.sendFile(path.join(__dirname+'/public/app/views/error.html'));
+});
+
+
+//start the app
+app.listen(config.port);
+
+console.log('Catalog app started at port '+config.port);
+
+//needed for testing to invoke app
+module.exports = app;
